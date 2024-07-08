3deb9a5b37a3efb588c7aa1f9776c86cd95c98a8
virgesmith <andrew@friarswood.net>
2024-07-08 09:22:50 +0100

    better webauthn example

---
+++ examples/webauthn_host.py
@@ -0,0 +1,69 @@
+import logging
+import struct
+from base64 import b64decode
+from datetime import datetime, timezone
+from hashlib import sha256
+from typing import Annotated
+
+import ecdsa
+from fastapi import FastAPI, Header, HTTPException
+
+logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
+
+userdata: dict[str, bytes] = {}
+
+CHALLENGE = "sign this!"
+HOST_ID = "http://localhost:8000"
+
+app = FastAPI()
+
+
+@app.get("/register")
+async def register(
+    username: Annotated[str, "user name"], pubkeyhex: Annotated[str, "short-form pulic key in hex"]
+) -> None:
+    """
+    Register a new user and their public key. Will fail if user already exists
+    """
+    global userdata
+    logging.info(f"registering {username} with {pubkeyhex}")
+    if username in userdata:
+        raise HTTPException(status_code=403, detail="user exists")
+    userdata[username] = bytes.fromhex(pubkeyhex)
+    return "OK"
+
+
+@app.get("/challenge")
+async def challenge() -> str:
+    """
+    Provide a challenge string for the authenticator to sign
+    """
+    return CHALLENGE
+
+
+@app.get("/login")
+async def login(username: Annotated[str, "user name"], token: Annotated[str, Header()]) -> str:
+    """
+    Authenticate with the host. Token is the signature of a combination of host, challenge and timestamp
+    """
+    logging.info(f"{username}: {token}")
+
+    if username not in userdata:
+        raise HTTPException(status_code=403, detail="user not found")
+
+    pubkey = userdata[username]
+    timestamp = datetime.now(tz=timezone.utc)
+    # check it verifies (using a 3rdparty library)
+    verifying_key = ecdsa.VerifyingKey.from_string(pubkey, curve=ecdsa.SECP256k1, hashfunc=sha256)
+
+    # append rounded timestamp to challenge
+    t = int(timestamp.timestamp() * 1000)
+    challenge = CHALLENGE.encode() + HOST_ID.encode() + struct.pack("Q", t - t % 60000)
+
+    try:
+        result = verifying_key.verify(b64decode(token), challenge, sigdecode=ecdsa.util.sigdecode_der)
+        logging.info(f"{HOST_ID} verified: {result}")
+    except ecdsa.keys.BadSignatureError:
+        raise HTTPException(status_code=401, detail="authentication failed") from None
+
+    return "OK"
--- examples/webauthn_interactive.py
+++ examples/webauthn_interactive.py
@@ -2,46 +2,44 @@ from datetime import datetime, timezone
 
 from pico_crypto_key.device import CryptoKey
 
-hosts = ["example.com", "othersite.net"]
+import requests
 
-
-def select_host() -> int:
-    print("Available hosts:")
-    for i, h in enumerate(hosts):
-        print(f"{i+1:>2d}. {h}")
-    while True:
-        raw = input()
-        if raw.isdigit() and 0 < int(raw) < len(hosts) + 1:
-            return hosts[int(raw) - 1]
-
+HOST = "http://localhost:8000"
+USER = "user1"
 
 def main() -> None:
-    global hosts
     with CryptoKey() as key:
         version, timestamp = key.info()
         print(f"{version} @ {timestamp}")
         while True:
-            match cmd := input("\nRegister/Auth/Quit? (r/a/q) "):
-                case "r":
-                    host = input("Host: ")
-                    pubkey = key.register(host)
-                    print(f"Registering with {host}: pubkey is {pubkey.hex()}")
-                    hosts.append(host)
-                case "a":
-                    _, timestamp = key.info()
-                    now = datetime.now(tz=timezone.utc)
-                    print(f"Host-device time diff: {(now - timestamp).total_seconds()}s")
+            try:
+                match cmd := input("\nRegister/Auth/Quit? (r/a/q) "):
+                    case "r":
+                        pubkey = key.register(HOST).hex()
+                        print(f"Registering {USER} with {HOST}: pubkey is {pubkey}")
+                        response = requests.get(f"{HOST}/register", params={"username": USER, "pubkeyhex": pubkey})
+                        response.raise_for_status()
+                        print(response.json())
+                    case "a":
+                        _, timestamp = key.info()
+                        now = datetime.now(tz=timezone.utc)
+                        print(f"Host-device time diff: {(now - timestamp).total_seconds()}s")
 
-                    host = select_host()
-
-                    challenge = input("Challenge string: ")
-                    token = key.auth(host, challenge.encode()).decode()
-                    print(f"Authenticating with {host} using challenge={challenge}")
-                    print(f"Token: {token}")
-                case "q":
-                    break
-                case _:
-                    print(f"Invalid input: {cmd}")
+                        response = requests.get(f"{HOST}/challenge")
+                        response.raise_for_status()
+                        challenge = response.json()
+                        token = key.auth(HOST, challenge.encode()).decode()
+                        print(f"Authenticating {USER} with {HOST} using challenge='{challenge}'")
+                        print(f"Token: {token}")
+                        response = requests.get(f"{HOST}/login", params={"username": USER}, headers={"token": token})
+                        response.raise_for_status()
+                        print(response.json())
+                    case "q":
+                        break
+                    case _:
+                        print(f"Invalid input: {cmd}")
+            except requests.exceptions.HTTPError as e:
+                print(e)
 
 
 if __name__ == "__main__":
