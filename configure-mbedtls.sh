bec199c62ac9893f4e2b17675fcdacb24287d61e
virgesmith <andrew@friarswood.net>
2026-05-11 12:54:38 +0100

    ...

--- configure-mbedtls.sh
+++ configure-mbedtls.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 [[ -n "$PICO_SDK_PATH" ]] || { echo "PICO_SDK_PATH not set"; exit 1; }
 
-echo Looking for mbedtls at $PICO_SDK_PATH/lib/mbedtls
+echo "Looking for mbedtls at $PICO_SDK_PATH/lib/mbedtls"
 
 [[ -d "${PICO_SDK_PATH}/lib/mbedtls" ]] || { echo "mbedtls not found"; exit 1; }
 
-cd $PICO_SDK_PATH/lib/mbedtls
+cd "$PICO_SDK_PATH/lib/mbedtls"
+
+echo Setting MBEDTLS config
 
 scripts/config.py unset MBEDTLS_TIMING_C
 scripts/config.py set MBEDTLS_NO_PLATFORM_ENTROPY
